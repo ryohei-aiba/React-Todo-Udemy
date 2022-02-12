5aadaedcec339ae7e3ed59287e91f176cdca2c78
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-02-12 15:54:42 +0900

    Create adding task function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["1", "2"]);
   const [completeTodos, setCompleteTodos] = useState(["1", "2"]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
